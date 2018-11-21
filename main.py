010dda901ea0a5c0e68095e1b4d073b866d28ba4
satoken-sato <44893375+satoken-sato@users.noreply.github.com>
2018-11-21 20:04:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -62,4 +62,4 @@ def draw_positions(frame, decoded_objs):
 
 
 if __name__ == '__main__':
-app.run(host="0.0.0.0", debug=False, threaded=True)
+    app.run(host="0.0.0.0", debug=False, threaded=True)
